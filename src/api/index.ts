b80eecc8ed72d7b277f13fbaaf0b0274e6f07d5a
vuphuthi <114985279+vuphuthi@users.noreply.github.com>
2023-07-07 01:35:12 +0700

    update-project

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,9 @@
-import axios from "axios";
-const instance = axios.create({
-    baseURL: "http://localhost:3000",
-    headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer token"
-    },
-    timeout: 3000
-})
-export default instance
+    import axios from "axios";
+    const instance = axios.create({
+        baseURL: "http://localhost:8080/api",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer token"
+        },
+    })
+    export default instance
